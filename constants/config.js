505ab270aec9061a9ecca551aad9debc00cd4700
Paras Dua <parasdua1130@gmail.com>
2024-07-21 23:10:36 +0530

    changes

--- constants/config.js
+++ constants/config.js
@@ -2,6 +2,7 @@ const corsOptions = {
     origin: [
         "http://localhost:5173",
         "http://localhost:4173",
+        "https://gupshup-client.vercel.app",
         process.env.CLIENT_URL,
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
