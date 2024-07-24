07600ef1d633408bf673d2a9b220e28103c82fff
Paras Dua <parasdua1130@gmail.com>
2024-07-24 11:07:04 +0530

    done

--- constants/config.js
+++ constants/config.js
@@ -1,12 +1,13 @@
 const corsOptions = {
-    origin: [
-        "http://localhost:5173",
-        "http://localhost:4173",
-        "https://gupshup-client.vercel.app",
-        process.env.CLIENT_URL,
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
+	origin: [
+		"http://localhost:5173",
+		"http://localhost:4173",
+		"https://gupshup-client.vercel.app",
+		"https://parasdua30.github.io/Gupshup/",
+		process.env.CLIENT_URL,
+	],
+	methods: ["GET", "POST", "PUT", "DELETE"],
+	credentials: true,
 };
 
 const GUPSHUP_TOKEN = "gupshup-token";
